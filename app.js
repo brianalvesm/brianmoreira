8540ac55c0d5bb32689037a7ac73690bba746b39
Brian Alves Moreira <brian.moreira@myfreecomm.com.br>
2019-11-15 02:59:12 -0300

    Add mysql db variables

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "mysql669.umbler.com",
+  user: "brian",
+  database: "brianmoreira"
+  password: "brianmoreiradb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
 var app = express();
 
 // view engine setup
