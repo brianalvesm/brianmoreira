b9b49a624b77ccf8ce659487589d8043ea3dac8d
Brian Alves Moreira <brian.moreira@myfreecomm.com.br>
2020-07-18 14:07:08 -0300

    Implementa suites animadas no setor web

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 	(function ($) {
 
-
+		setInterval(function(){ 
+			$(".suite-animated").toggleClass('suite-animated-milkyway');
+			$(".suite-animated").toggleClass('suite-animated-andromeda'); 
+		}, 5000);
 
 	})(jQuery);
 });
