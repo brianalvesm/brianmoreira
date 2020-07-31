a80a75563fbd9b464c63009e903f89c5f257c6d2
Brian Alves Moreira <brian.moreira@myfreecomm.com.br>
2020-07-31 11:58:10 -0300

    Cria tooltip no input e hover nos vídeos

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	(function ($) {
 
-		 $(document).foundation();
+		$(document).foundation();
 
 		setInterval(function(){ 
 			$(".suite-animated").toggleClass('suite-animated-milkyway');
@@ -20,50 +20,50 @@ $(document).ready(function(){
 			);
 		});
 
-  // Select all links with hashes
-  $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
-      // On-page links
-      if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-        && 
-        location.hostname == this.hostname
-      ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        // Does a scroll target exist?
-        if (target.length) {
-          // Only prevent default if animation is actually gonna happen
-          event.preventDefault();
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 2500, function() {
-            // Callback after animation
-            // Must change focus!
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) { // Checking if the target was focused
-              return false;
-            } else {
-              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-              $target.focus(); // Set focus again
-            };
-          });
+    // Select all links with hashes
+    $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+          && 
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 2500, function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
+          }
         }
-      }
-    });
+      });
 
-  $(".has-tooltip").hover(
-  	function() {
-  		$(this).find(".tooltip").addClass('show');
-  	}, function() {
-  		$(this).find(".tooltip").removeClass('show');
-  	}
-  );
+    $(".has-tooltip").hover(
+    	function() {
+    		$(this).find(".tooltip").addClass('show');
+    	}, function() {
+    		$(this).find(".tooltip").removeClass('show');
+    	}
+    );
 
 	})(jQuery);
 });
