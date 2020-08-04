075f4ff89f747469b7c84d1669b459522cb44a60
Brian Alves Moreira <brian.moreira@myfreecomm.com.br>
2020-08-04 17:07:45 -0300

    Melhorias de SEO; Remoção de página sobre de teste

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Brian Moreira' });
+  res.render('index', { title: 'Brian Moreira - Designer & Desenvolvedor front-end freelancer aprendendo ilustração e animação 2D' });
 });
 
 /* GET about page. */
-router.get('/sobre', function(req, res, next) {
-  res.render('about', { title: 'Sobre | Brian Moreira' });
-});
+// router.get('/sobre', function(req, res, next) {
+//   res.render('about', { title: 'Sobre | Brian Moreira' });
+// });
 
 module.exports = router;
