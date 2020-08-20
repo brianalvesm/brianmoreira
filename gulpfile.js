7fba37a050b1164f4a147b5a746be605a660aed1
Brian Alves Moreira <hey@brianmoreira.com>
2020-08-20 13:24:14 -0300

    Install uglify

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,10 @@ const gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	notify = require('gulp-notify'),
 	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
 	cleanCSS = require('gulp-clean-css'),
-	autoprefixer = require('gulp-autoprefixer');
+	autoprefixer = require('gulp-autoprefixer'),
+	pipeline = require('readable-stream').pipeline;
 
 sass.compiler = require('node-sass');
 
@@ -22,8 +24,17 @@ gulp.task('minify-css', () => {
     .pipe(gulp.dest('./public/stylesheets/dist/'));
 });
 
+gulp.task('compress', function () {
+  return pipeline(
+    gulp.src('./public/js/**/*.js'),
+    uglify(),
+    concat('scripts.min.js'),
+    gulp.dest('./public/js/dist/')
+  );
+});
+
 gulp.task('watch', function () {
   gulp.watch('./public/sass/**/*.sass', gulp.series('sass'));
 });
 
-gulp.task('default', gulp.series('watch', 'sass', 'minify-css'));
+gulp.task('default', gulp.series('watch', 'sass', 'minify-css', 'compress'));
