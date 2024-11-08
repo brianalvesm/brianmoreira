8045e3aec88bd925be607517b942aa4653d637ad
brianalvesm <hey@brianmoreira.com>
2024-11-07 21:36:00 -0300

    Teste de script

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,18 @@ $(document).ready(function(){
 
 		$(document).foundation();
 
+    $(function(){
+      $("#header").load("../header.html"); 
+      $("#footer").load("../footer.html"); 
+    });
+
+    if ($("body").hasClass("page-about")) {
+      $('.nav-about').addClass('active');
+    };
+
+    if ($("body").hasClass("page-contact")) {
+      $('.nav-contact').addClass('active');
+    };
+
 	})(jQuery);
 });
