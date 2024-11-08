57dc3ad9a0f124c880a6e633ca382931c0de917f
brianalvesm <hey@brianmoreira.com>
2024-11-08 13:04:39 -0300

    Sets timeout to had active class to nav

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,13 @@ $(document).ready(function(){
 		$(document).foundation();
 
     function loadPartials() {
-      console.log("First Function start");
 
       $("#header").load("../header.html");
       $("#footer").load("../footer.html");
 
-      console.log("First Function end");
     };
 
     function activeNav() {
-      console.log("Second Function start");
 
       if ($("body").hasClass("page-illustration")) {
         $('.nav-illustration').addClass('active');
@@ -27,12 +24,11 @@ $(document).ready(function(){
         $('.nav-contact').addClass('active');
       };
 
-      console.log("Second Function end");
     };
 
     loadPartials();
 
-    setTimeout(activeNav, 1000);
+    setTimeout(activeNav, 500);
 
 
 	})(jQuery);
