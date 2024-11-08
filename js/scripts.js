4a3c38d97fe13a706ec06df913422e25f9bf0fdc
brianalvesm <hey@brianmoreira.com>
2024-11-07 21:27:19 -0300

    Cria script para adicionar classe ativa

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,5 @@ $(document).ready(function(){
 
 		$(document).foundation();
 
-    if ($("body").hasClass("page-about")) {
-      $('.nav-about').addClass('active');
-    };
-
-    if ($("body").hasClass("page-contact")) {
-      $('.nav-contact').addClass('active');
-    };
-
 	})(jQuery);
 });
