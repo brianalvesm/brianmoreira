d3f9eaafc3c5fa7322d7a047e0b26e0e756e7876
brianalvesm <hey@brianmoreira.com>
2024-11-08 12:33:26 -0300

    Teste jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,31 @@
-$("#header").load("../header.html"); 
-$("#footer").load("../footer.html");
-
 $(document).ready(function(){
 	(function ($) {
 
 		$(document).foundation();
 
-    if ($("body").hasClass("page-illustration")) {
-      $('.nav-illustration').addClass('active');
-    };
+    console.log("First Function start");
 
-    if ($("body").hasClass("page-about")) {
-      $('.nav-about').addClass('active');
-    };
+    $("#header").load("../header.html");
+    $("#footer").load("../footer.html");
 
-    if ($("body").hasClass("page-contact")) {
-      $('.nav-contact').addClass('active');
+    console.log("First Function end");
+
+    function activeNav() {
+      console.log("Second Function start");
+
+      if ($("body").hasClass("page-illustration")) {
+        $('.nav-illustration').addClass('active');
+      };
+
+      if ($("body").hasClass("page-about")) {
+        $('.nav-about').addClass('active');
+      };
+
+      if ($("body").hasClass("page-contact")) {
+        $('.nav-contact').addClass('active');
+      };
+
+      console.log("Second Function end");
     };
 
 	})(jQuery);
