937edb582c5bd59fbe03e17e3c277bbe8475ec32
brianalvesm <hey@brianmoreira.com>
2024-11-07 22:02:23 -0300

    Teste de script

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,24 @@
+$("#header").load("../header.html"); 
+$("#footer").load("../footer.html");
+
 $(document).ready(function(){
 	(function ($) {
 
 		$(document).foundation();
 
-    $("#header").load("../header.html"); 
-    $("#footer").load("../footer.html");
+    console.log( "ready!" );
 
-    $(function() {
-      console.log( "ready!" );
-      if ($("body").hasClass("page-about")) {
-        $('.nav-about').addClass('active');
-      };
+    if ($("body").hasClass("page-illustration")) {
+      $('.nav-illustration').addClass('active');
+    };
 
-      if ($("body").hasClass("page-contact")) {
-        $('.nav-contact').addClass('active');
-      };
-    });
+    if ($("body").hasClass("page-about")) {
+      $('.nav-about').addClass('active');
+    };
+
+    if ($("body").hasClass("page-contact")) {
+      $('.nav-contact').addClass('active');
+    };
 
 	})(jQuery);
 });
