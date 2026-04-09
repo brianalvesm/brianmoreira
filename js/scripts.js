ce737e182f48227ddaa5e806f6e89b3092f32a20
brianalvesm <hey@brianmoreira.com>
2026-04-09 10:01:19 -0300

    Ajustes gerais

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
     });
 
     $(function () {
-      var images = ['tower-dragon-1.png', 'tower-dragon-2.png', 'bears-bakery-1.png', 'bears-bakery-2.png', 'fantasy-autumn-1.png', 'fantasy-autumn-2.png', 'pirate-ship-1.png', 'pirate-ship-2.png', 'future-me-1.png', 'future-me-2.png', 'future-me-3.png', 'oddi.png', 'snowman.png'];
+      var images = ['tower-dragon-1.png', 'tower-dragon-2.png', 'bears-bakery-1.png', 'bears-bakery-2.png', 'fantasy-autumn-1.png', 'fantasy-autumn-2.png', 'pirate-ship-1.png', 'pirate-ship-2.png', 'future-me-1.png', 'future-me-3.png', 'oddi.png', 'snowman.png'];
       $('.contact-profiles').css({
         'background-image': 'url(../images/profiles/' + images[Math.floor(Math.random() * images.length)] + ')'
       });
